8ec63e26a00612ae723feafc14b6d3ec2da654b3
PairoseParveen <fairose.0507@gmail.com>
2016-09-12 10:21:18 +0530

    Add files via upload

---
+++ ideone_VTqMRM.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int n;
+	printf("Enter n");
+	scanf("%d",&n);
+	if (n%2==0)
+	printf("The no %d is even",n);
+	else
+	printf("The no %d is odd",n);
+	return 0;
+}
